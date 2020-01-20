d344c53b4d71b6b33a74fc84250b7d206302d051
-Unknown- <MrUnknownYT@gmail.com>
2020-01-20 05:52:06 -0700

    Small fix & Loaded message

--- UnknownLibs/src/main/java/mrunknown404/unknownlibs/Main.java
+++ UnknownLibs/src/main/java/mrunknown404/unknownlibs/Main.java
@@ -1,12 +1,19 @@
 package mrunknown404.unknownlibs;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Main.MOD_ID, useMetadata = true)
+@Mod(modid = Main.MOD_ID, useMetadata = true, acceptableRemoteVersions = "*")
 public class Main {
 	public static final String MOD_ID = "unknownlibs";
 	
 	@Instance
 	public static Main main;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent e) {
+		System.out.println("Loaded UnknownLibs Version: " + e.getModMetadata().version);
+	}
 }
