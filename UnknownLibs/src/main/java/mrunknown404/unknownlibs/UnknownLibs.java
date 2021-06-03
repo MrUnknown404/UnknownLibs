063b0e7a4bdbdf4a15cceaf898995065517d13b4
-Unknown- <mrunknownyt@gmail.com>
2021-06-03 14:23:33 -0600

    Base

---
+++ UnknownLibs/src/main/java/mrunknown404/unknownlibs/UnknownLibs.java
@@ -0,0 +1,12 @@
+package mrunknown404.unknownlibs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(UnknownLibs.MOD_ID)
+public class UnknownLibs {
+	public static final String MOD_ID = "unknownlibs";
+	
+	public UnknownLibs() {
+		
+	}
+}
